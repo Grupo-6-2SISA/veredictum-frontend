197de65cb0802f5e26e9e40d128197d381120042
manuelamrodrigues <manuela.rodrigues@sptech.school>
2025-10-17 20:19:49 -0300

    Feat: modificações na home com tailwind

--- veredictum/src/pages/Home/Home.jsx
+++ veredictum/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import efeito from './../../assets/svg/efeito.svg';
 import logo from './../../assets/svg/logo_vectorized.svg';
 import imgHome from './../../assets/img/img-home.jpeg';
 import "./Home.css";
+import "./../../styles/typography.css"
 
 
 function Home() {
@@ -12,38 +13,38 @@ function Home() {
     <div className="min-h-screen bg-[#222] flex items-center justify-center">
 
       <div className="w-full max-w-[1400px] bg-white rounded-lg flex flex-col lg:flex-row h-auto lg:h-[600px] shadow-lg">
-        
-        <div className="w-full h-full lg:w-1/2 flex flex-col">
-          <img src={efeito} alt="Efeito decorativo" className="w-30" />
 
-          <div className="logo-row flex items-start mb-3">
+        <div className="w-full h-full lg:w-1/2 flex flex-col">
+          <img src={efeito} alt="Efeito decorativo" className="w-25" />
+
+          <div className="logo-row flex items-center gap-2 mb-4 ml-3 mt-2">
             <img
               src={logo}
               alt="Veredictum"
-              className="w-1/10 lg:w-1/16 h-auto mr-20 lg:mr-6 "
+              className="w-12 h-auto "
             />
-            <span className="text-xl font-bold text-[#222] flex items-center justify-center">Veredictum</span>
+            <span className="text-xl font-bold text-[#222] ">Veredictum</span>
           </div>
 
-          <div className="home-boas-vindas flex flex-col ml-4 gap-5">
-            <p className="text-xl font-bold text-[#222]">
+          <div className="flex flex-col ml-4 gap-5">
+            <span className="text-3xl font-bold text-[#222] mt-1">
               Boas-Vindas
-            </p>
-            <p className="text-[#555] text-base lg:text-lg">
+            </span>
+            <span className="text-[#555] text-base lg:text-lg">
               Veredictum: Gestão do Escritório de Advocacia
-            </p>
+            </span>
 
             {/* divider matches vanilla: 180px x 8px */}
             <div className="w-[180px] h-2 lg:h-2 bg-[#FFC300] rounded" />
 
-            <p className="text-[#333]">
+            <span className="text-[#333]">
               Para acessar a plataforma, cadastre-se ou faça login com a conta existente
-            </p>
+            </span>
 
-            <div className="flex gap-[8px] ml-[60px] mt-[40px]">
+            <div className="flex gap-[8px] ml-[60px] mt-4">
               <button
                 onClick={() => navigate('/login')}
-                className="btn-login  bg-[#111] text-white px-8 lg:px-10 py-5 lg:py-2 rounded-xl shadow hover:bg-[#d1d1d1] hover:text-black transition"
+                className="btn-login border-2 bg-[#111] text-white w-full lg:w-48 lg:py-2 rounded-xl shadow hover:bg-[#d1d1d1] hover:text-black transition"
                 style={{ height: '48px' }}
               >
                 Login
